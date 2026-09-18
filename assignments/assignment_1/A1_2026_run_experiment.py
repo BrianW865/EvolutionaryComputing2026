44cdf52b8269d03670bc0a9df23b1464e16a7b77
BrianW865 <156130383+BrianW865@users.noreply.github.com>
2026-09-18 15:45:15 +0200

    Update A1_2026_run_experiment.py

--- assignments/assignment_1/A1_2026_run_experiment.py
+++ assignments/assignment_1/A1_2026_run_experiment.py
@@ -13,18 +13,16 @@ console = Console()
 
 SEEDS = [0,1,2,3,4]
 NUM_OF_GENERATIONS = 10
+NUM_OF_CHILDREN_PER_GEN = 10
 POPULATION_SIZE = 20
-EVALUATION_BUDGET = POPULATION_SIZE + 10 * NUM_OF_GENERATIONS
+EVALUATION_BUDGET = POPULATION_SIZE + NUM_OF_CHILDREN_PER_GEN * NUM_OF_GENERATIONS
 
 def main() -> None:
     for seed in SEEDS:
         console.log(f"Running for Seed: {seed}")
         run_EA1 = run_EA1(seed, NUM_OF_GENERATIONS, POPULATION_SIZE)
         run_EA2 = run_EA2(seed, NUM_OF_GENERATIONS, POPULATION_SIZE)
-
-        evaluation_budget = max(run_EA1.num_evaluations, run_EA2.num_evaluations)
-
-        run_random = random_search(seed, evaluation_budget, _)
+        run_random = random_search(seed, EVALUATION_BUDGET)
 
         #log the results
         #make a graph for each seed including EA1, EA2 and random_search
